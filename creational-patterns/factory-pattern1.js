49992fe9b0237726672e05142d9a22d5284ddf86
LAKSHMIKANT <lakshmikant4u@gmail.com>
2022-12-28 01:46:18 +0530

    grouping

--- creational-patterns/factory-pattern1.js
+++
@@ -1,9 +0,0 @@
-function garmentFactory(type, material, color) {
-    return {
-        type, material, color
-    }
-}
-
-const shirt = garmentFactory("shirt", "nylon", "blue");
-const pant = garmentFactory("pant", "jeans", "black");
-
--- creational-patterns/factory-pattern2.js
+++
@@ -1,8 +0,0 @@
-function chocolatefactory(type, shape, flavour ) {
-    return {
-        type, shape, flavour,
-    }
-}
-
-const chocolava = chocolatefactory("dark chocolate", "round", "strawberry");
-const chocofill = chocolatefactory("while chocolate", "heart", "vanilla");
